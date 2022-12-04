135a8d5493742027ab2802f9930d7114f5c3af9b
Kelvin Zhao <yunxi.kelvin@gmail.com>
2022-12-03 19:52:16 -0800

    remove items

--- ICS 32/lab3/test.py
+++
@@ -1,8 +0,0 @@
-import pathlib
-
-path_object = pathlib.Path().cwd()
-
-path_object.open("textfile.txt", "w")
-
-with open(path_object/pathlib.Path("newfile.txt"), "w") as new_file:
-    new_file.write("hi")
--- ICS 32/lab4/moretesting.py
+++
@@ -1,7 +0,0 @@
-def func(input: str):
-    print(input)
-
-x = 10
-y = 4
-
-func(str(f"{x}, {y}"))
